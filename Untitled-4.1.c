fe516c6842584da2346d9b4d0b73b19bf4976b75
anastasiapolianek-ops <anastasia.polianek@gmail.com>
2026-03-30 15:43:29 +0300

    Implement input validation for x and z
    Added input validation for variables x and z, ensuring non-negative values and preventing division by zero.

--- Untitled-4.1.c
+++ Untitled-4.1.c
@@ -2,7 +2,33 @@
 #include <math.h>
 
 int main() {
-    int x = 0, y = 10;
-    
-        return 0;
+    int x, z;
+    double y;
+
+    while (1) {
+        printf("Введіть x і z: ");
+        scanf("%d %d", &x, &z);
+
+        if (x < 0) {
+            printf("Помилка: корінь з від'ємного x. Введіть інші значення.\n\n");
+            continue;
+        }
+
+        if (x * z < 0) {
+            printf("Помилка: корінь з від'ємного x * z. Введіть інші значення.\n\n");
+            continue;
+        }
+
+        if (sqrt(x) - 2 * sqrt(x * z) == 0) {
+            printf("Помилка: ділення на 0. Введіть інші значення.\n\n");
+            continue;
+        }
+
+        break;
+    }
+
+    y = (sqrt(x) + 2 * sqrt(x * z)) / (sqrt(x) - 2 * sqrt(x * z));
+    printf("y = %lf\n", y);
+
+    return 0;
 }
